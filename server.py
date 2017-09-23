82486cf51a65587512eb4b336f49684e77e3a085
yogesh <yogesh.yog07@gmail.com>
2017-09-23 20:42:37 +0530

    user reg half

--- server.py
+++ server.py
@@ -1,23 +1,51 @@
 import os
 import socket 
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
+from Crypto.Cipher import AES
+from Crypto.Util import number
+from Crypto.Hash import SHA256
 
 host='localhost'
 port=5000
+x=os.urandom(16)
+p=number.getPrime(256)
+h1=SHA256.new()
+h2=SHA256.new()
+h2_dash=SHA256.new()
+
+def encrypt(msg):
+	iv=os.urandom(16)
+	obj = AES.new(x, AES.MODE_CBC,iv)
+	ct = obj.encrypt(msg)
+	return ct
+
+class SmartCard:
+	def __init__(self,did,v0):
+		self.did=did
+		self.v0=v0
 
 class Server:
 	def __init__(self):
 		pass
 
-	def register(self,id):
-		backend = default_backend()
-		key = os.urandom(32)
-		iv = os.urandom(16)
-		cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-		encryptor = cipher.encryptor()
-		ct = encryptor.update(id) + encryptor.finalize()
-		return ct	
+	def register(self,id_u):
+		id_s=bytearray(os.urandom(16))
+		ci=bytearray(os.urandom(16))
+		n0=bytearray(os.urandom(16))
+		id=bytearray()
+		for i in range(16):
+			id.append(id_u[i]|id_s[i]|ci[i])
+		tmp1=bytearray()
+		for i in range(16):
+			tmp1.append(id[i]|n0[i])	
+		did=encrypt(bytes(tmp1))
+		did=bytearray(did)
+		tmp2=bytearray()
+		for i in range(16):
+			tmp2.append(id[i]|x[i])	
+		h1.update(bytes(tmp2))
+		v0=bytearray(h1.digest())
+		sc=SmartCard(did,v0)
+		return sc
 
 if __name__=="__main__":
 	server=Server()
@@ -25,8 +53,9 @@ if __name__=="__main__":
 	sock.bind((host,port))
 	sock.listen(1)
 	conn,addr=sock.accept()
-	id=conn.recv(1024)
-	enc=server.register(id)
-	conn.send(enc)
+	id_u=conn.recv(1024)
+	id_u=bytearray(id_u)
+	sc=server.register(id_u)
+	conn.send(sc)
 	conn.close()
 	sock.close()
--- user.py
+++ user.py
@@ -5,16 +5,23 @@ host=socket.gethostbyname(socket.gethostname())
 port=5000
 server='localhost'
 
+class SmartCard:
+	def __init__(self,did=None,v0=None):
+		self.did=did
+		self.v0=v0
+	
 class User:
 	def __init__(self):
-		self.id=os.urandom(16)
+		self.id_u=bytearray(os.urandom(16))
 
 	def register(self):
 		sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		sock.connect((server,port))
-		sock.send(self.id)
-		enc=sock.recv(1024)
-		print(enc)
+		sock.send(self.id_u)
+		sc=SmartCard()
+		sc=sock.recv(1024)
+		print(sc.did)
+		print(sc.v0)
 		sock.close()	
 
 

c238ead150bd34e2cdbab5577eda456507b0a8ef
yogesh <yogesh.yog07@gmail.com>
2017-10-12 22:12:46 +0530

    login complete

--- server.py
+++ server.py
@@ -2,13 +2,14 @@ import os
 import socket 
 import pickle
 import struct
+import sys
 from time import time
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 
 host='localhost'
 port=5000
-x=os.urandom(16)
+x=os.urandom(32)
 iv=os.urandom(16)
 h1=SHA256.new()
 h2=SHA256.new()
@@ -32,25 +33,27 @@ class SmartCard:
 		self.v0=v0
 		self.ctr_sc=ctr_sc
 		self.n=n
-	
+	def increment_counter(self):
+		self.ctr_sc=self.ctr_sc+1
+
 class Server:
 	def __init__(self):
 		pass
 
 	def register(self,id_u):
-		id_s=bytearray(os.urandom(16))
-		ci=bytearray(os.urandom(16))
-		n0=bytearray(os.urandom(16))
+		id_s=bytearray(os.urandom(32))
+		ci=bytearray(os.urandom(32))
+		n0=bytearray(os.urandom(32))
 		id=bytearray()
-		for i in range(16):
+		for i in range(32):
 			id.append(id_u[i]|id_s[i]|ci[i])
 		tmp1=bytearray()
-		for i in range(16):
+		for i in range(32):
 			tmp1.append(id[i]|n0[i])		
 		did=encrypt(bytes(tmp1))
 		did=bytearray(did)
 		tmp2=bytearray()
-		for i in range(16):
+		for i in range(32):
 			tmp2.append(id[i]|x[i])	
 		h1.update(bytes(tmp2))
 		v0=bytearray(h1.digest())
@@ -59,20 +62,35 @@ class Server:
 		id=bytearray(id)
 		return (did,v0,tmp1,id)
 
+	def authenticate(self,sk):
+		sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		sock.bind((host,port))
+		sock.listen(1)
+		conn,addr=sock.accept()
+		flg=conn.recv(1).decode()
+		conn.close()
+		sock.close()
+		if flg=='1':
+			print("The session key is {0}".format(sk))
+			return 1
+		else:
+			reg_table[id][1]=reg_table[id][1]+1		
+			return 0
+
 	def login(self,sc,tmp1,id,v0,id_u):
 		sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		sock.bind((host,port))
 		sock.listen(1)
 		conn,addr=sock.accept()
-		v1=conn.recv(16)
-		did=conn.recv(16)
-		t1=conn.recv(16)
-		g=conn.recv(16)
+		v1=conn.recv(32)
+		did=conn.recv(32)
+		t1=conn.recv(32)
+		g=conn.recv(32)
 		conn.close()
 		sock.close()	
 		t2=bytearray(struct.pack(">i",int(time())))
 		fresh=1
-		for i in range(12):
+		for i in range(28):
 				t2.append(0)
 		for i in range(4):
 			if t1[i]==t2[i]:
@@ -82,41 +100,41 @@ class Server:
 			else:
 				continue
 		if fresh==0:
-			print("Aborting")
+			print("Aborting because timestamp is not fresh")
 			sys.exit(1)
 		tmp2=bytearray(decrypt(did))
 		did=bytearray(did)
-		if tmp1==tmp2 and reg_table[id][1]<sc.n:
+		if reg_table[id][1]<sc.n:
 			tmp3=bytearray()
-			for i in range(16):
+			for i in range(32):
 				tmp3.append(v0[i]|id_u[i]|t1[i])
 			h1.update(bytes(tmp3))
 			tmp4=bytearray(h1.digest())
 			tmp5=bytearray()
-			for i in range(16):
+			for i in range(32):
 				tmp5.append((tmp4[i]*g[i])%256)	
 			e=bytearray()
-			for i in range(16):
-				e.append((v1[i]-tmp5[i])%256)
-			u=os.urandom(16)
-			n1=os.urandom(16)
+			for i in range(32):
+				e.append((v1[i]-tmp5[i])%256)	
+			u=os.urandom(32)
+			n1=os.urandom(32)
 			c=bytearray()
-			for i in range(16):
-				c.append((u[i]*e[i])%256)
+			for i in range(32):
+				c.append((u[i]*e[i])%256)	
 			d=bytearray()
-			for i in range(16):
+			for i in range(32):
 				d.append((u[i]*g[i])%256)
 			tmp6=bytearray()
-			for i in range(16):
+			for i in range(32):
 				tmp6.append(id[i]|n1[i])
 			nid=encrypt(bytes(tmp6))
 			h2.update(bytes(c))
 			tmp7=h2.digest()
 			v2=bytearray()
-			for i in range(16):
+			for i in range(32):
 				v2.append(tmp7[i]^nid[i])
 			tmp8=bytearray()
-			for i in range(16):
+			for i in range(32):
 				tmp8.append(nid[i]|c[i]|t2[i])
 			h2.update(tmp8)
 			v3=h2.digest()
@@ -129,8 +147,22 @@ class Server:
 			sock.send(t2)
 			sock.close()
 		else:
-			print("Aborintg")
-			sys.exit(1)									
+			print("Aborting")
+			sys.exit(1)	
+		sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		sock.bind((host,port))
+		sock.listen(1)
+		conn,addr=sock.accept()
+		v4=conn.recv(32)
+		sk=conn.recv(32)
+		conn.close()
+		sock.close()
+		tmp9=bytearray()
+		for i in range(32):
+			tmp9.append(v1[i]|c[i])
+		h2.update(tmp9)
+		tmp10=bytearray(h2.digest())
+		return self.authenticate(sk)
 
 if __name__=="__main__":
 	server=Server()
@@ -146,4 +178,6 @@ if __name__=="__main__":
 	conn.send(pickle.dumps(sc))
 	conn.close()
 	sock.close()
-	server.login(sc,tmp1,id,v0,id_u)
+	ctr=0
+	while (ctr<sc.n) and (not server.login(sc,tmp1,id,v0,id_u)):
+		ctr=ctr+1
--- user.py
+++ user.py
@@ -2,6 +2,7 @@ import os
 import socket
 import pickle
 import struct
+import sys
 from time import time
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
@@ -19,10 +20,12 @@ class SmartCard:
 		self.v0=v0
 		self.ctr_sc=ctr_sc
 		self.n=n
+	def increment_counter(self):
+		self.ctr_sc=self.ctr_sc+1	
 	
 class User:
 	def __init__(self):
-		self.id_u=bytearray(os.urandom(16))
+		self.id_u=bytearray(os.urandom(32))
 
 	def register(self):
 		sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -31,44 +34,71 @@ class User:
 		sc=SmartCard()
 		data=sock.recv(1024)
 		sc=pickle.loads(data)
-		print("Enter a 16-character password:")
+		print("Enter an 8-character password:")
 		pw=input()
 		pw=bytearray(pw.encode())
+		for i in range(24):
+			pw.append(0)
 		h1.update(bytes(pw))
 		tmp1=bytearray(h1.digest())
 		v0=sc.v0
 		v=bytearray()
-		for i in range(16):
+		for i in range(32):
 			v.append(sc.v0[i]^tmp1[i])
 		sc.v0=v
 		sc.ctr_sc=0
 		sc.n=3
 		sock.close()	
-		return sc,v0
+		return sc,v0,pw
 
-	def login(self,sc,v0):
+	def authenticate(self,sc,sk,pd):
+		sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		sock.connect((server,port))
 		print("Enter the password:")
 		pw=input()
 		pw=bytearray(pw.encode())	
+		for i in range(24):
+			pw.append(0)
+		if pw==pd:
+			flg='1'
+			sock.send(flg.encode())
+			sock.close()
+			print("Login is successful")
+			print("The session key is {0}".format(sk))
+			print("\n")
+		else:
+			flg='0'
+			sock.send(flg.encode())
+			sock.close()
+			sc.increment_counter()
+			if sc.ctr_sc==sc.n:
+				print("Aborting")
+				sys.exit(1)
+			else:
+				print("Login unsuccessful")
+				print("{0} attempts remaining".format(sc.n-sc.ctr_sc))
+				print("\n")		
+
+	def login(self,sc,v0,pd):
 		if sc.ctr_sc<sc.n:
-			r=bytearray(os.urandom(16))
-			g=bytearray(os.urandom(16))
+			r=bytearray(os.urandom(32))
+			g=bytearray(os.urandom(32))
 			e=bytearray()
-			for i in range(16):
-				e.append((r[i]*g[i])%256)
+			for i in range(32):
+				e.append((r[i]*g[i])%256)	
 			tmp1=bytearray()
 			t1=bytearray(struct.pack(">i",int(time())))
-			for i in range(12):
+			for i in range(28):
 				t1.append(0)
-			for i in range(16):
+			for i in range(32):
 				tmp1.append(v0[i]|self.id_u[i]|t1[i])
 			h1.update(bytes(tmp1))
 			tmp2=bytearray(h1.digest())
 			tmp3=bytearray()
-			for i in range(16):
+			for i in range(32):
 				tmp3.append((tmp2[i]*g[i])%256)
 			v1=bytearray()
-			for i in range(16):
+			for i in range(32):
 				v1.append((tmp3[i]+e[i])%256)
 			sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 			sock.connect((server,port))
@@ -81,22 +111,77 @@ class User:
 			sock.bind((server,port))
 			sock.listen(1)
 			conn,addr=sock.accept()
-			v2=conn.recv(16)
+			v2=conn.recv(32)
 			v3=conn.recv(32)
-			d=conn.recv(16)
-			t2=conn.recv(16)
+			d=conn.recv(32)
+			t2=conn.recv(32)
 			conn.close()
 			sock.close()
 			v2=bytearray(v2)
 			v3=bytearray(v3)
 			d=bytearray(d)
-			t2=bytearray(t2)	
+			t2=bytearray(t2)
+			t1=bytearray(struct.pack(">i",int(time())))
+			fresh=1
+			for i in range(28):
+					t1.append(0)
+			for i in range(4):
+				if t1[i]==t2[i]:
+					continue
+				elif abs(t1[i]-t2[i])>100:
+					fresh=0
+				else:
+					continue
+			if fresh==0:
+				print("Aborting")
+				sys.exit(1)
+			c=bytearray()	
+			for i in range(32):
+				c.append((r[i]*d[i])%256)	
+			h2.update(bytes(c))
+			tmp4=bytearray(h2.digest())
+			nid=bytearray()
+			for i in range(32):
+				nid.append(v2[i]^tmp4[i])
+			tmp5=bytearray()
+			for i in range(32):
+				tmp5.append(nid[i]|c[i]|t2[i])	
+			tmp6=bytearray()
+			for i in range(32):
+				tmp6.append(v1[i]|c[i])
+			h2.update(tmp6)
+			v4=bytearray(h2.digest())
+			sc.did=nid
+			tmp7=bytearray()
+			for i in range(32):
+				tmp7.append(c[i]|d[i]|e[i])
+			h2.update(tmp7)
+			sk=h2.digest()			
+			sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			sock.connect((server,port))
+			sock.send(v4)
+			sock.send(sk)
+			sock.close()
+			self.authenticate(sc,sk,pd)
 
 if __name__=="__main__":
 	user=User()
 	sc=SmartCard()
-	sc,v0=user.register()
-	print("User has been registered")
-	user.login(sc,v0)
+	while 1:
+		print("1.Register")
+		print("2.Login")
+		print("3.Change password")
+		print("4.Revoke smart card")
+		print("5.Exit")
+		print("\n")
+		print("Enter your choice:")
+		ch=int(input())
+		if ch==1:
+			sc,v0,pw=user.register()
+			print("User has been registered"+"\n")
+		elif ch==2:	
+			user.login(sc,v0,pw)
+		else:
+			break	
 
 			
